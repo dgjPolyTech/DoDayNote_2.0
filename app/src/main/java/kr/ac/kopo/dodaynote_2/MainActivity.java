9d3a2b4b1f6b9d7b8e59be733cfbc6b3d1d5ab0e
dgjPolyTech <dgjpolytech@gmail.com>
2026-05-06 20:59:34 +0900

    기본 및 세부 ui intent 구조 구축

--- app/src/main/java/kr/ac/kopo/dodaynote_2/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/dodaynote_2/MainActivity.java
@@ -1,6 +1,9 @@
 package kr.ac.kopo.dodaynote_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView habit_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        habit_1 = findViewById(R.id.habit_1);
+        habit_1.setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, HabitDetailActivity.class);
+
+            startActivity(intent);
+        }
+    };
 }
