683bc58fbd9c526f5689703ea01210d1bab46536
dgjPolyTech <dgjpolytech@gmail.com>
2026-05-16 21:31:42 +0900

    자잘한 수정 사항 반영

--- app/src/main/java/kr/ac/kopo/dodaynote_2/SettingActivity.java
+++ app/src/main/java/kr/ac/kopo/dodaynote_2/SettingActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.kopo.dodaynote_2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class SettingActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinnerLanguage = findViewById(R.id.spinner_language);
+        String[] languageOptions = {"한국어", "English"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, languageOptions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLanguage.setAdapter(adapter);
     }
 }
